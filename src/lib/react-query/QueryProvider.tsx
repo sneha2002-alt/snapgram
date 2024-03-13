b38c99b82b0213a29ee120ef87edfbc6b6e188f4
sneha <snehamittal.sm22@gmail.com>
2024-03-13 16:36:59 +0530

    first commit

---
+++ src/lib/react-query/QueryProvider.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+export const QueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
